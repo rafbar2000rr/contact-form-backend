98f49b7e5f98caf6e562e822098f581a4283c25f
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-07-28 23:57:35 -0500

    cambio

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/Contact');
+const Contact = require('../models/contacts');
 const nodemailer = require('nodemailer');
 
 // 🔹 Obtener todos los contactos

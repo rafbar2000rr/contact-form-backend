b280cd02e5113329d591d7dda17a19a56b6dc32b
Rafael Ibarra <rafbar2000rr@gmail.com>
2025-07-31 00:20:33 -0500

    cambio

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ require('dotenv').config(); // 🔹 Esto debe ir al inicio para usar las variabl
 
 const contactRoutes = require('./routes/contacts'); // 🔹 Importa tus rutas
 
-const app = express();
+const app = express(); // la función express() devuelve un objeto app que representa tu servidor web.
 
-app.use(cors());
-app.use(express.json()); // 🔹 Necesario para que entienda JSON
-
+app.use(cors()); //Permite que cualquier origen pueda acceder al backend.
+app.use(express.json()); // le dice al servidor Express que acepte y procese automáticamente datos en 
+                         //formato JSON que lleguen en el cuerpo (body) de las peticiones HTTP (como POST o PUT)
 // 🔹 Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)// conecta la aplicación a tu base de datos MongoDB.
+
+
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 // 🔹 Usa las rutas
-app.use('/api/contacts', contactRoutes);
+app.use('/api/contacts', contactRoutes);//le dice a Express:“Cada vez que llegue una solicitud que comience 
+                                    // con /api/contacts, usa las rutas que están definidas en contactRoutes.”
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
